651d5b3abd1095cebbc9b1d38a119886bf85a8b6
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-10 12:43:33 +0900

    fix(error): 型宣言を追加し、コンストラクタの引数を修正

--- backend/src/models/response/error.php
+++ backend/src/models/response/error.php
@@ -25,11 +25,11 @@ class Details {
 }
 
 class Error {
-  public $details;
-  public $message;
-  public $code;
+  public Details $details;
+  public string | null $message = "Something went wrong";
+  public int $code = 400;
 
-  public function __construct(Details $details, string $message = "Something went wrong", $code = null) {
+  public function __construct(Details $details, string $message = "Something went wrong", int $code = 400) {
     $this->details = $details;
     $this->message = $message;
     $this->code = $code;
@@ -44,3 +44,11 @@ class Error {
   }
 }
 
+enum ErrorCode: int {
+  case BAD_REQUEST = 400;
+  case UNAUTHORIZED = 401;
+  case FORBIDDEN = 403;
+  case NOT_FOUND = 404;
+  case UNPROCESSABLE_ENTITY = 422;
+  case INTERNAL_SERVER_ERROR = 500;
+}
