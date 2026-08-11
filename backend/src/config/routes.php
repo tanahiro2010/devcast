2358ab4691e16096a1d695175d2b3791b79f6750
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-11 18:48:26 +0900

    refactor(routes): すでにapiのサブドメインで運用していたため、APIグループを削除し、/healthエンドポイントを直接ルートに追加

--- backend/src/config/routes.php
+++ backend/src/config/routes.php
@@ -7,7 +7,5 @@ require __DIR__ . '/../futures/health/health.controller.php';
 $v1_routes = null;
 
 $routes = new Routes([
-  Route::group('/api', [
-    Route::get('/health', [new HealthController(), 'health'])
-  ])
+   Route::get('/health', [new HealthController(), 'health'])
 ]);
