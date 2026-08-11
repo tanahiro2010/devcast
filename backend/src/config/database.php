2153bd42cda69527bf000bfa41af3103cdb77220
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-11 20:20:03 +0900

    feat(database): データベース接続にポート設定を追加

--- backend/src/config/database.php
+++ backend/src/config/database.php
@@ -5,6 +5,7 @@ $capsule = new Capsule();
 $capsule->addConnection([
   'driver' => 'mysql',
   'host' => getenv('DB_HOST') ?: 'localhost',
+  'port' => getenv('DB_PORT') ?: 3306,
   'database' => getenv('DB_NAME') ?: 'devcast',
   'username' => getenv('DB_USER') ?: 'admin',
   'password' => getenv('DB_PASSWORD') ?: 'admin',
