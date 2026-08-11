d3fa6844855cff4221ca691433977ccdf442b22e
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-11 20:10:12 +0900

    feat(database): データベース接続のためのmysqliインスタンスを追加

--- backend/src/config/database.php
+++ backend/src/config/database.php
@@ -3,3 +3,5 @@ define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
 define('DB_NAME', getenv('DB_NAME') ?: 'devcast');
 define('DB_USER', getenv('DB_USER') ?: 'admin');
 define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'admin');
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
