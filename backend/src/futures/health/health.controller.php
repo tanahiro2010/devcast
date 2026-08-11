9f4d8579fb996e1a913897b61c09bca1e687bc13
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-11 15:48:14 +0900

    feat(health): HealthControllerを追加し、ヘルスチェックエンドポイントを実装

---
+++ backend/src/futures/health/health.controller.php
@@ -0,0 +1,10 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class HealthController {
+  public function health(Request $_, Response $response): Response {
+    $response->getBody()->write('Hello, World!');
+    return $response;
+  }
+}
