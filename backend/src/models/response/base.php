f9da6504cfcf8a8f0c9621b47ca14a1cb09730b1
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-12 16:10:21 +0900

    refactor(models): 成功時responseでもDetailsを返したいのでDetailsをBaseネームスペースに分離

---
+++ backend/src/models/response/base.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Models\Response\Base;
+
+class Details {
+  public $instance;
+  public $date;
+  public $status;
+
+  public function __construct(string $instance, string | null $date = null, int $status = 400) {
+    if ($date === null) {
+      $date = date('Y-m-d H:i:s');
+    }
+    $this->instance = $instance;
+    $this->date = $date;
+    $this->status = $status;
+  }
+
+  public function toArray() {
+    return array(
+      'instance' => $this->instance,
+      'date' => $this->date,
+      'status' => $this->status
+    );
+  }
+}
--- backend/src/models/response/error.php
+++ backend/src/models/response/error.php
@@ -1,28 +1,6 @@
 <?php
 namespace App\Models\Response;
-
-class Details {
-  public $instance;
-  public $date;
-  public $status;
-
-  public function __construct(string $instance, string | null $date = null, int $status = 400) {
-    if ($date === null) {
-      $date = date('Y-m-d H:i:s');
-    }
-    $this->instance = $instance;
-    $this->date = $date;
-    $this->status = $status;
-  }
-
-  public function toArray() {
-    return array(
-      'instance' => $this->instance,
-      'date' => $this->date,
-      'status' => $this->status
-    );
-  }
-}
+use App\Models\Response\Base\Details;
 
 class Error {
   public Details $details;
