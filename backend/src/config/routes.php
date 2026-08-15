e00090a58c90e38ba03ef6d6c368147b847395d9
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-15 10:58:07 +0900

    feat(routes): ルートに認証関連のコントローラーを追加

--- backend/src/config/routes.php
+++ backend/src/config/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Tanahiro2010\SlimRouterDsl\Routes;
@@ -8,10 +9,18 @@ define('__BASE_CONTROLLER_PATH__', __DIR__ . '/../futures');
 
 require __BASE_CONTROLLER_PATH__ . '/errors/controller.php';
 require __BASE_CONTROLLER_PATH__ . '/health/controller.php';
+require __BASE_CONTROLLER_PATH__ . '/auth/controller.php';
+require __BASE_CONTROLLER_PATH__ . '/auth/callback/controller.php';
 
 $v1_routes = null;
 
 $routes = new Routes([
    Route::get('/', [new errorsController(), 'notFound']),
-   Route::get('/health', [new HealthController(), 'health'])
+   Route::get('/health', [new HealthController(), 'health']),
+   Route::group('/auth', [
+      Route::controller(new AuthController(), [
+         Route::get('/', 'oauthUrl')
+      ]),
+      Route::get('/callback', [new GithubCallbackController(), 'callback'])
+   ]),
 ]);
