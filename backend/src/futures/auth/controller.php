2e5236a0ad7490849ee41590d1cde8cf391fcbc1
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-15 10:57:56 +0900

    fix(auth): require文をrequire_onceに変更して重複読み込みを防止

--- backend/src/futures/auth/controller.php
+++ backend/src/futures/auth/controller.php
@@ -2,8 +2,8 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-require __DIR__ . '/../../helpers/response.php';
-require __DIR__ . '/../../config/env.php';
+require_once __DIR__ . '/../../helpers/response.php';
+require_once __DIR__ . '/../../config/env.php';
 require __DIR__ . '/service.php';
 
 class AuthController
