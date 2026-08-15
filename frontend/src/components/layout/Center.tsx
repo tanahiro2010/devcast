6788c9427a23c794ac551eb2ab53515680b3aaa1
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-15 12:45:59 +0900

    fix(layout): classNameプロパティをオプショナルに変更

--- frontend/src/components/layout/Center.tsx
+++ frontend/src/components/layout/Center.tsx
@@ -2,7 +2,7 @@ import { cn } from "../../lib/utils"
 
 type Props = {
   children: React.ReactNode
-  className: string
+  className?: string
 }
 
 const Center = ({ children, className = "" }: Props) => (
