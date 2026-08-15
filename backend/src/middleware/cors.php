050d2b1a97eda9b4f2ab15a90dd271b7b915f6f3
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-15 11:25:24 +0900

    feat(middleware): CORSミドルウェアを追加してCORSヘッダーを設定

---
+++ backend/src/middleware/cors.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Middleware;
+
+use App\Config\Config;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class CorsMiddleware implements MiddlewareInterface {
+  public function process(Request $request, RequestHandler $handler): Response {
+    $config = Config::frontend();
+    // Set CORS headers
+    header("Access-Control-Allow-Origin: " . $config['base_url']);
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+    // Handle preflight requests
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+      http_response_code(200);
+      exit();
+    }
+
+    return $handler->handle($request);
+  }
+}
