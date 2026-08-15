359c906557a7f8d6ed4191e3984cef2f516e0351
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-15 10:52:24 +0900

    fix(response): エラーレスポンスにコードを追加し、エラーメッセージの処理を改善

--- backend/src/helpers/response.php
+++ backend/src/helpers/response.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../models/response/success.php';
 
 use App\Models\Response\Base\Details as ErrorDetails;
 use App\Models\Response\Status as ErrorStatus;
+use App\Models\Response\Code as ErrorCode;
 use App\Models\Response\Error as ErrorResponse;
 use App\Models\Response\Success as SuccessResponse;
 
@@ -38,19 +39,19 @@ class ApiResponseHelper {
   }
 
 
-  static function sendErrorResponse(Response $response, string $instance, ErrorStatus $status = ErrorStatus::BAD_REQUEST, string $message = "Something went wrong") {
+  static function sendErrorResponse(Response $response, string $instance, string $message = "Something went wrong", ErrorStatus $status = ErrorStatus::BAD_REQUEST, ErrorCode $code = ErrorCode::SOMETHING_WENT_WRONG) {
     $errorDetails = new ErrorDetails($instance, null, $status->value);
-    $errorResponse = new ErrorResponse($errorDetails, $message, $status);
+    $errorResponse = new ErrorResponse($errorDetails, $message, $code);
     return self::sendResponse($response, $status->value, $errorResponse->toArray());
   }
   
-  static function errorResponse(Response $response, ErrorStatus $errorStatus, string $instance, ?string $message = null) {
+  static function errorResponse(Response $response, ErrorStatus $errorStatus, ErrorCode $code, string $instance, ?string $message = null) {
     $status = $errorStatus->value;
     $config = RESPONSE_CONFIG[$status] ?? array('message' => 'Something went wrong');
     $defaultMessage = $config['message'];
     $errorMessage = $message ?? $defaultMessage;
 
-    return self::sendErrorResponse($response, $instance, $errorStatus, $errorMessage);
+    return self::sendErrorResponse($response, $instance, $errorMessage, $errorStatus, $code);
   }
 
   static function sendSuccessResponse(Response $response, mixed $data, string $message = "Success", int $status = 200) {
