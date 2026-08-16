26d489b0dc7efacb77bd0d38509bd9aef42880c9
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-16 13:19:37 +0900

    fix(config): oauthメソッド内のリダイレクトURIの取得方法を修正

--- backend/src/config/env.php
+++ backend/src/config/env.php
@@ -14,13 +14,14 @@ class Config {
   }
 
   public static function oauth(): array {
+    $config = self::server();
     if (self::$oauth === null) {
       self::$oauth = [
         'github' => [
           'client_id' => self::env('GITHUB_CLIENT_ID') ?: '',
           'client_secret' => self::env('GITHUB_CLIENT_SECRET') ?: '',
-          'redirect_uri' => self::env('GITHUB_REDIRECT_URI') ?: 'http://localhost:8000/auth/callback',
-          'front_redirect_uri' => self::server()['frontend']['base_url'] . '/_auth/callback',
+          'redirect_uri' => $config['backend']['base_url'] . '/auth/callback',
+          'front_redirect_uri' => $config['frontend']['base_url'] . '/_auth/callback',
           'scope' => ["read:user", "user:email"],
         ],
         'providers' => ['github'],
