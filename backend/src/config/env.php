d6924596122638f8d623cd402acd3fef2e84bca0
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-16 13:11:22 +0900

    feat(config): oauthにfront_redirect_uriを追加

--- backend/src/config/env.php
+++ backend/src/config/env.php
@@ -20,6 +20,7 @@ class Config {
           'client_id' => self::env('GITHUB_CLIENT_ID') ?: '',
           'client_secret' => self::env('GITHUB_CLIENT_SECRET') ?: '',
           'redirect_uri' => self::env('GITHUB_REDIRECT_URI') ?: self::frontend()['base_url'] . '/_auth/callback',
+          'front_redirect_uri' => self::frontend()['base_url'] . '/_auth/callback',
           'scope' => ["read:user", "user:email"],
         ],
         'providers' => ['github', 'google', 'qiita'],
