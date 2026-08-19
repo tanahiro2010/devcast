d4e71322acccae65b141986240bed4aaf46285ae
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-19 22:27:46 +0900

    feat(App): tanstack/react-queryを導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouteProvider } from '@util-tools/react-router-dsl'
 import { routes } from './config/routes'
 import './App.css'
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      retry: 2,
+    }
+  },
+})
+
 function App() {
-  return <RouteProvider routes={routes} />
+  return (
+    <QueryClientProvider client={client}>
+      <RouteProvider routes={routes} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
 
 export default App
