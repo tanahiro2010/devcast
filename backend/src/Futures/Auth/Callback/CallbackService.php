b6d870564c2bbfc26076c8793f0d0914d81f35e1
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-19 18:20:01 +0900

    feat(CallbackService): findOrCreateUserメソッドを追加し、ユーザーの取得または作成を実装

--- backend/src/Futures/Auth/Callback/CallbackService.php
+++ backend/src/Futures/Auth/Callback/CallbackService.php
@@ -3,6 +3,7 @@ namespace App\Futures\Auth\Callback;
 
 use App\Libraries\GitHub;
 use App\Config\Config;
+use App\Models\DB\User;
 
 class CallbackService {
   private GitHub $github;
@@ -28,5 +29,22 @@ class CallbackService {
 
   public function getProfile(string $accessToken): array {
     return $this->github->getProfile($accessToken);
+  }
+
+  public function findOrCreateUser(string $provider, array $profile): User {
+    $user = User::findByProviderId($provider, $profile['id']);
+    
+    if (!$user) {
+      $user = User::create([
+        'username' => $profile['login'],
+        'email' => $profile['email'] ?? null,
+        'name' => $profile['name'] ?? "unknown",
+        'provider' => $provider,
+        'provider_id' => $profile['id'],
+        'avatar_url' => $profile['avatar_url'] ?? null,
+      ]);
+    }
+
+    return $user;
   }
 }
