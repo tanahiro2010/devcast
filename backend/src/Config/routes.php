7c6ba2a12ccbb47137906a63eb4829ce8f06d210
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-19 23:38:52 +0900

    refactor(routes): CorsMiddlewareを削除し、AuthMiddlewareを適用

--- backend/src/Config/routes.php
+++ backend/src/Config/routes.php
@@ -1,7 +1,8 @@
 <?php
+
 use Tanahiro2010\SlimRouterDsl\Routes;
 use Tanahiro2010\SlimRouterDsl\Route;
-use App\Middleware\CorsMiddleware;
+use App\Middleware\AuthMiddleware;
 use App\Futures\Errors\ErrorsController;
 use App\Futures\Health\HealthController;
 use App\Futures\Auth\AuthController;
@@ -9,14 +10,16 @@ use App\Futures\Auth\Callback\CallbackController;
 
 
 $routes = new Routes([
-  Route::middleware(new CorsMiddleware(), [
-    Route::get('/', [new ErrorsController(), 'notFound']),
-    Route::get('/health', [new HealthController(), 'health']),
-    Route::group('/auth', [
-      Route::controller(new AuthController(), [
-        Route::get('/', 'oauthUrl')
+  Route::get('/', [new ErrorsController(), 'notFound']),
+  Route::get('/health', [new HealthController(), 'health']),
+  Route::group('/auth', [
+    Route::controller(new AuthController(), [
+      Route::get('/', 'oauthUrl'),
+      Route::middleware(new AuthMiddleware(), [
+        Route::post('/refresh', 'refresh'),
       ]),
-      Route::get('/callback', [new CallbackController(), 'callback'])
-   ]),
+    ]),
+
+    Route::get('/callback', [new CallbackController(), 'callback'])
   ]),
 ]);
