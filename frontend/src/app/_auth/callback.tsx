9260678f65916176ac1644a1a590692403b888f9
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-19 22:28:11 +0900

    feat(auth): コールバック処理を追加し、トークンの取得とリダイレクトを実装

--- frontend/src/app/_auth/callback.tsx
+++ frontend/src/app/_auth/callback.tsx
@@ -1,9 +1,34 @@
-import { Loading } from "../../components/screen/Loading";
+import { useQuery } from "@tanstack/react-query"
+import { redirect, useSearchParams } from "react-router-dom"
+import { useEffect } from "react"
+import { client } from "../../lib/api"
+import { Loading } from "../../components/screen/Loading"
+
 
 const Callback = () => {
+  const [searchParams, _] = useSearchParams();
+  const { isPending, error } = useQuery({
+    queryKey: ["auth/callback"],
+    queryFn: async () => {
+      const token = searchParams.get('token')
+      if (!token) throw new Error('トークンが設定されていません')
+      sessionStorage.setItem('access_token', token)
 
+      const result = await client.auth.getRefreshToken()
+      return localStorage.setItem('refresh_token', result)
+    }
+  })
 
-  return <Loading />
+  useEffect(() => {
+    redirect('/')
+  }, [isPending])
+
+  if (isPending) return <Loading />
+  else if (error) return (
+    <div>
+      Error
+    </div>
+  )
 }
 
-export default Callback;
+export default Callback
