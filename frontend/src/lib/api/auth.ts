d1eba48e95d926eb059ef8d8c73fd0629fb4242b
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-21 22:09:02 +0900

    fix(auth): getRefreshTokenメソッドの戻り値を修正し、アクセストークンを返すように変更

--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -10,17 +10,17 @@ class AuthApi {
     return data.data.url
   }
 
-  async getRefreshToken(): Promise<string> {
-    const response = await apiFetch('/api/refresh', {
+  async getRefreshToken(): Promise<{ refreshToken: string, accessToken: string }> {
+    const response = await apiFetch('/auth/refresh', {
       method: 'POST'
     })
     const data = await response.json()
 
-    return data.refresh_token
+    return { refreshToken: data.refresh_token, accessToken: data.access_token }
   }
 
   async getAccessToken(): Promise<string> {
-    const response = await apiFetch('/api/access', {
+    const response = await apiFetch('/access', {
       method: 'POST',
       body: JSON.stringify({ refresh_token: localStorage.getItem('refresh_token') })
     })
