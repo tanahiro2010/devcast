feb2cb664bd98a9f25f822dccddec5be64692e89
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-23 20:15:04 +0900

    feat(migrations): サブスクリプションテーブル作成のマイグレーションを追加

---
+++ backend/src/database/migrations/003_create_subscriptions_table.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
+
+
+return [
+    'up' => function (Capsule $capsule) {
+        //
+    },
+    'down' => function (Capsule $capsule) {
+        //
+    },
+];
