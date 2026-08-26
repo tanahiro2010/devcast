52a1dbe8c0453bd714387a5c0866555dc87ccc0a
tanahiro2010 <herentongkegu087@gmail.com>
2026-08-26 21:11:18 +0900

    feat(api): Clientクラスにv1クライアントを統合

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,7 +1,9 @@
 import { AuthApi } from "./auth"
+import { client as v1Client } from "./v1"
 
 class Client {
   readonly auth = new AuthApi()
+  readonly v1 = v1Client
 }
 
 const client = new Client()
